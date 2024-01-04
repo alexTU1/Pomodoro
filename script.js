1de16efb62993bc8866bb84b18dca451a28c6ae9
Alexander Commodore <acommo1@students.towson.edu>
2024-01-04 13:01:22 -0800

    working pause button, adding helpful comments to code

--- script.js
+++ script.js
@@ -2,29 +2,55 @@ let minutes = document.getElementById('mins');
 let seconds = document.getElementById('secs');
 
 let minsToWork = 25;
-let secsToWork = "00";
+//total amount of seconds
+let time = minsToWork * 60;
+let secsToWork = time%60;
 
+//adding zero placeholder
+if(secsToWork < 10) {
+    secsToWork = '0' + secsToWork ;
+}
+
+//time is set to 25:00 on window load and reload
 window.onload = () => {
-    minutes.innerHTML = minsToWork;
+    minutes.innerHTML = Math.floor(time/60);
     seconds.innerHTML = secsToWork;
 }
 
-function play(){
-    let mins = 0;
-    let secs = 9;
+let mins = minsToWork - 1;
+let secs = 59;
+let interval;
 
+/**
+ * (1) Minutes goes to 24, secs goes to 59
+ * (2) Secs value decrease every second
+ * (3) Mins value decrease every time secs === 0
+ */
+function play(){
     let timeHandler = () => {
         seconds.innerHTML = secs;
         minutes.innerHTML = mins;
-        secs = secs - 1;
+        secs--;
         if(secs === 0){
             mins = mins - 1;
             secs = 59;
+        }
+        if(secs < 10) {
+            secs = '0' + secs ;
         }
         if(mins < 0){
             mins = 0;
         }
     }
-
-    setInterval(timeHandler, 1000);
+      interval = setInterval(timeHandler, 1000);  
 }
+
+/**
+ * Pauses setInterval time
+ */
+function pause(){
+    clearInterval(interval);
+ }
+
+
+
